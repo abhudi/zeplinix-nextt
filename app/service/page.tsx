1e60e3a974769fe1833a40aca4444f678a873db9
Abhishek Deshmukh <deshmukhabhishek7020@gmail.com>
2024-12-10 19:44:09 +0530

    service page done changes

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -6,37 +6,37 @@ export default function Service() {
     return (
       <div>
         <div className="w-full bg-[#303030] text-white p-6 sm:p-10 md:p-14 rounded-3xl shadow-lg mt-10 lg:mt-32 ">
-          <h2 className="text-fs-24 sm:text-fs-34 lg:text-fs-54 font-bold mb-4 sm:mb-6 leading-tight pb-10 text-center md:text-left">
+          <h2 className="text-fs-24 sm:text-fs-34 lg:text-fs-54 font-semibold mb-4 sm:mb-6 leading-tight pb-10 text-center md:text-left">
             Benefit of Working with us
             <span className="inline-block bg-red-600 w-3 h-3 rounded-full mx-1"></span>
           </h2>
 
           <div className="space-y-6">
-            <div className="flex flex-col md:flex-row md:items-start gap-4 border-b border-[#8E8E8E] pb-5">
+            <div className="flex flex-col md:flex-row md:items-start gap-4 border-b border-[#6d6d6d] pb-5">
               <h3 className="text-base sm:text-lg font-normal md:w-[50%] text-center md:text-left">
                 Time zones ain’t no thing
               </h3>
-              <p className="text-sm sm:text-base text-gray-400 md:w-[50%] text-center md:text-left">
+              <p className="text-sm sm:text-base text-[#909090] md:w-[50%] text-center md:text-left">
                 Wherever you are in the world, you’ll feel like we’re right
                 around the corner. With 12 years of experience, our business
                 processes are seamless and time differences don’t matter.
               </p>
             </div>
-            <div className="flex flex-col md:flex-row md:items-start gap-4 border-b border-[#8E8E8E] pb-5">
+            <div className="flex flex-col md:flex-row md:items-start gap-4 border-b border-[#6d6d6d] pb-5">
               <h3 className="text-base sm:text-lg font-normal md:w-[50%] text-center md:text-left">
                 Impossible? We’re on it
               </h3>
-              <p className="text-sm sm:text-base text-gray-400 md:w-[50%] text-center md:text-left">
+              <p className="text-sm sm:text-base text-[#909090] md:w-[50%] text-center md:text-left">
                 Wherever you are in the world, you’ll feel like we’re right
                 around the corner. With 12 years of experience, our business
                 processes are seamless and time differences don’t matter.
               </p>
             </div>
-            <div className="flex flex-col md:flex-row md:items-start gap-4 border-b border-[#8E8E8E] pb-5">
+            <div className="flex flex-col md:flex-row md:items-start gap-4 border-b border-[#6d6d6d] pb-5">
               <h3 className="text-base sm:text-lg font-normal md:w-[50%] text-center md:text-left">
                 Flexible work terms
               </h3>
-              <p className="text-sm sm:text-base text-gray-400 md:w-[50%] text-center md:text-left">
+              <p className="text-sm sm:text-base text-[#909090] md:w-[50%] text-center md:text-left">
                 Wherever you are in the world, you’ll feel like we’re right
                 around the corner. With 12 years of experience, our business
                 processes are seamless and time differences don’t matter.
@@ -46,7 +46,7 @@ export default function Service() {
               <h3 className="text-base sm:text-lg font-normal md:w-[50%] text-center md:text-left">
                 Full spectrum of services
               </h3>
-              <p className="text-sm sm:text-base text-gray-400 md:w-[50%] text-center md:text-left">
+              <p className="text-sm sm:text-base text-[#909090] md:w-[50%] text-center md:text-left">
                 Wherever you are in the world, you’ll feel like we’re right
                 around the corner. With 12 years of experience, our business
                 processes are seamless and time differences don’t matter.
@@ -64,7 +64,7 @@ export default function Service() {
         title: "Application Services",
         icon: "/service/icons/flutter.png",
         content:
-          "Covers the full lifecycle of software applications, from development and deployment to ongoing maintenance...",
+          "Covers the full lifecycle of software applications, from development and deployment to ongoing maintenance. Our focus is on ensuring optimal performance, security, and user experience, while continuously adapting to meet your needs.",
         link: "/application", // Update link
         image: "/service/flutter.png",
       },
@@ -111,7 +111,7 @@ export default function Service() {
     ];
 
     return (
-      <div className="relative grid gap-6 mt-32 mx-4 lg:mx-9 mb-32">
+      <div className="relative grid gap-12 mt-32 mx-4 lg:mx-9 mb-32">
         {services.map((service, index) => (
           <ServiceCard key={index} {...service} />
         ))}
