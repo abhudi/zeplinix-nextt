ba1db3bfa620ded6d5b9416bdae3f4e374bd4e37
Abhishek Deshmukh <deshmukhabhishek7020@gmail.com>
2024-12-24 11:36:58 +0530

    updated push for verify website

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-import { NextConfig } from "next";
+// @ts-check
 
-/** @type {NextConfig} */
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // output: "export", // This generates static HTML files for hosting
-  images: {
-    unoptimized: true, // Required for static export if using the Image component
-  },
-  trailingSlash: true, // Ensures proper file paths for static hosting
-  assetPrefix: process.env.NODE_ENV === "production" ? "./" : undefined, // Adjust paths for static export
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
